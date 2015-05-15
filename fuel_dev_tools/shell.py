7d3ff3cf045a63033479e6bdcd91e89f8f42fd45
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-05-15 11:46:57 +0200

    Slaves mcollective rsync
    Change-Id: I30130f0d1d4fb1bf3b2f8264ea3d5b411ad99bcd

--- fuel_dev_tools/shell.py
+++ fuel_dev_tools/shell.py
@@ -32,6 +32,7 @@ from fuel_dev_tools.docker import nailgun
 from fuel_dev_tools.docker import nginx
 from fuel_dev_tools.docker import postgres
 from fuel_dev_tools.docker import rabbitmq
+from fuel_dev_tools.slaves import mcagent
 from fuel_dev_tools import exc
 from fuel_dev_tools import info
 from fuel_dev_tools import ssh
@@ -53,6 +54,8 @@ COMMANDS = {
     'astute-stop': astute.Stop,
     'astute-tail': astute.Tail,
     'astute-volumes': astute.Volumes,
+
+    'mcagent-rsync': mcagent.Rsync,
 
     'nailgun-id': nailgun.Id,
     'nailgun-config': nailgun.Config,
---
+++ fuel_dev_tools/slaves/mcagent.py
@@ -0,0 +1,54 @@
+import os
+
+from fuel_dev_tools import command
+from fuel_dev_tools import rsync
+from fuel_dev_tools import slaves
+from fuel_dev_tools import ssh
+
+
+class Rsync(slaves.SlavesMixin,
+            ssh.SSHMixin,
+            rsync.RsyncMixin,
+            command.BaseCommand):
+
+    def get_parser(self, prog_name):
+        parser = super(Rsync, self).get_parser(prog_name)
+
+        parser.add_argument(
+            '-s', '--source',
+            nargs='?',
+            default='.',
+            help='Source of the rsync-ed directory.'
+        )
+
+        return parser
+
+    def target_for_slave(self, slave):
+        if slave['status'] in ['discover', 'error']:
+            return '/usr/libexec/mcollective/mcollective/agent'
+
+        raise Exception(
+            'Don\'t know target for status {status} for node {name}'.format(
+                **slave)
+        )
+
+    def take_action(self, parsed_args):
+        for slave in self.discover_slaves():
+            self.print_debug('Syncing to slave {name} [{ip}]'.format(**slave))
+
+            source = parsed_args.source
+            target = ':{}'.format(self.target_for_slave(slave))
+
+            hop_args = [
+                '-e',
+                'ssh -A -t root@{} -p {} ssh -A -t root@{}'.format(
+                    self.app_args.ip,
+                    self.app_args.port,
+                    slave['ip']
+                )
+            ]
+
+            self.rsync(source, target, *hop_args)
+
+            self.print_debug('Restarting mcollective')
+            self.slave_command(slave, '/etc/init.d/mcollective', 'restart')
